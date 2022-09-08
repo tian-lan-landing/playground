7299792ed81727a3c99713c0d8741e7d58c23511
Lan Tian <tian.lan@landing.ai>
2022-09-08 10:27:05 +0800

    cube loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import styles from './Loading.module.css';
 import cx from 'class-names';
 
-const Loading: React.FC<{ loading: boolean, grey?: boolean }> = props => {
-  const { loading, grey } = props;
+type FaceColor = {
+  left?: string;
+  right?: string;
+  top?: string;
+  bottom?: string;
+  front?: string;
+  back?: string;
+};
+
+const Loading: React.FC<{ loading: boolean, grey?: boolean, transform?: string, faceColor?: FaceColor }> = props => {
+  const { loading, grey, transform, faceColor } = props;
   return (
     <>
       <div className={cx(styles["cube-loading-wrapper"], loading && styles.loading, grey && styles.grey)}>
-        <div className={styles["cube-loading"]}>
-          <div className={styles["front"]}></div>
-          <div className={styles["back"]}></div>
-          <div className={styles["right"]}></div>
-          <div className={styles["left"]}></div>
-          <div className={styles["top"]}></div>
-          <div className={styles["bottom"]}></div>
+        <div className={styles["cube-loading"]} style={transform ? { transform } : undefined}>
+          <div className={styles["front"]} style={{ backgroundColor: faceColor?.front }}>F</div>
+          <div className={styles["back"]} style={{ backgroundColor: faceColor?.back }}>B</div>
+          <div className={styles["right"]} style={{ backgroundColor: faceColor?.right }}>R</div>
+          <div className={styles["left"]} style={{ backgroundColor: faceColor?.left }}>L</div>
+          <div className={styles["top"]} style={{ backgroundColor: faceColor?.top }}>T</div>
+          <div className={styles["bottom"]} style={{ backgroundColor: faceColor?.bottom }}>D</div>
         </div>
       </div>
     </>
--- pages/CubeLoading.tsx
+++ pages/CubeLoading.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Loading from '../components/Loading';
 
+const topColor = '#E9F5FE';
+const leftColor = '#BAC9FF';
+const frontColor = '#92ABFB';
+
 const CubeLoading: React.FC<{}> = () => {
-  return <Loading loading grey />;
+  const options = [
+    {
+      cube: 'rotateX(-30deg) rotateY(45deg)',
+      faceColor: { left: leftColor, top: topColor, front: frontColor },
+    },
+    {
+      // 12.5%, 25%
+      cube: 'rotateX(-30deg) rotateY(45deg) rotateZ(0.25turn)',
+      faceColor: { left: topColor, bottom: leftColor, front: frontColor },
+    },
+    {
+      // 37.5%, 50%
+      cube: 'rotateX(-30deg) rotateY(45deg) rotateZ(0.25turn) rotateX(90deg)',
+      faceColor: { left: topColor, bottom: frontColor, back: leftColor },
+    },
+    {
+      // 62.5%, 75%
+      cube: 'rotateX(-30deg) rotateY(45deg) rotateZ(0.25turn) rotateX(90deg) rotateZ(-0.25turn)',
+      faceColor: { left: frontColor, top: topColor, back: leftColor },
+    },
+    {
+      // 87.5%, 99%
+      cube: 'rotateX(-30deg) rotateY(45deg) rotateZ(0.25turn) rotateX(90deg) rotateZ(-0.25turn) rotateY(-90deg)',
+      faceColor: { left: leftColor, top: topColor, front: frontColor },
+    },
+  ];
+  const [transform, setTransform] = useState(options[0]);
+  return <>
+    {/* <div><input onBlur={e => setTransform(e.target.value)} defaultValue={transform} style={{ width: '100%' }} /></div> */}
+    <ul style={{ marginTop: '80px' }}>
+      {options.map(o => (
+        <li key={o.cube} onClick={() => setTransform(o)} style={{ backgroundColor: o.cube === transform.cube ? '#ddd' : 'white' }}>{o.cube}</li>
+      ))}
+    </ul>
+    <Loading loading grey transform={transform.cube} faceColor={transform.faceColor} />
+  </>;
 }
 
 export default CubeLoading;
