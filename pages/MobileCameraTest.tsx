ba30c5d32a4a13d75e78370fc7a9f1992393634d
Lan Tian <tian.lan@landing.ai>
2023-08-18 09:02:17 +0800

    Add codes to the page

--- pages/MobileCameraTest.tsx
+++ pages/MobileCameraTest.tsx
@@ -4,16 +4,19 @@ const MobileCameraTest: React.FC<{}> = () => {
       <div>
         <h1>Camera</h1>
         <p>Expected: open camera</p>
+        <pre>&lt;input type=&quot;file&quot; accept=&quot;image/*&quot; capture=&quot;environment&quot; /&gt;</pre>
         <input type="file" accept="image/*" capture="environment" />
       </div>
       <div>
         <h1>Album</h1>
         <p>Expected: open album on android</p>
+        <pre>&lt;input type=&quot;file&quot; accept=&quot;image/*&quot; /&gt;</pre>
         <input type="file" accept="image/*" />
       </div>
       <div>
         <h1>Camera + Album</h1>
         <p>Expected: both camera and album are supported</p>
+        <pre>&lt;input type=&quot;file&quot; accept=&quot;image/*;capture=camera&quot; /&gt;</pre>
         <input type="file" accept="image/*;capture=camera" />
       </div>
     </div>
